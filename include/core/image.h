456abd50d3263f5f540e75489134ea9157e64116
sumincho22 <scyclone.cho@gmail.com>
2021-04-06 23:46:20 -0500

    Added documentation for Image header file

--- include/core/image.h
+++ include/core/image.h
@@ -5,8 +5,17 @@
 
 namespace naivebayes {
 
+/**
+ * This class holds data for an Image.
+ */
 class Image {
  public:
+  /**
+   * Creates an Image object with the given label and pixel shades.
+   *
+   * @param label   the class label of the Image
+   * @param pixels  the pixel shades of the Image
+   */
   Image(size_t label, const std::vector<std::vector<size_t>>& pixels);
 
   size_t GetLabel() const;
