8a792635b088fcc9bf9381a9132580991f0a1aaf
sumincho22 <scyclone.cho@gmail.com>
2021-04-14 17:29:42 -0500

    Implemented test for operator>> overload in Model class

--- tests/test_model.cc
+++ tests/test_model.cc
@@ -41,6 +41,7 @@ TEST_CASE("Train", "[train]") {
   }
 }
 
+// NOTE: Make sure to empty test_file.txt before running this test
 TEST_CASE("operator<<", "[op<<]") {
   naivebayes::Data data(kTestSize);
   std::ifstream input_file("../../../data/testdata_size3.txt");
@@ -57,6 +58,32 @@ TEST_CASE("operator<<", "[op<<]") {
   test_file >> first_value;
 
   REQUIRE(first_value == kTestSize);
+}
+
+// NOTE: Data is already saved on the file this text is using
+TEST_CASE("operator>>", "[op>>]") {
+  naivebayes::Model model(3);
+  std::ifstream is;
+  is.open("../../../data/test_load_file.txt");
+  is >> model;
+
+  SECTION("Check prior probabilities after load") {
+    REQUIRE(model.GetPriorProb(1) == Approx(3.0/14.0).epsilon(.01));
+    REQUIRE(model.GetPriorProb(7) == Approx(2.0/14.0).epsilon(.01));
+    REQUIRE(model.GetPriorProb(0) == Approx(2.0/14.0).epsilon(.01));
+  }
+
+  SECTION("Check feature probabilities after load") {
+    REQUIRE(model.GetFeatureProb(0, 0, kUnshaded, 1) == Approx(1.0/2.0).epsilon(.01));
+    REQUIRE(model.GetFeatureProb(0, 1, kUnshaded, 1) == Approx(1.0/4.0).epsilon(.01));
+    REQUIRE(model.GetFeatureProb(0, 2, kUnshaded, 1) == Approx(3.0/4.0).epsilon(.01));
+    REQUIRE(model.GetFeatureProb(1, 0, kUnshaded, 1) == Approx(3.0/4.0).epsilon(.01));
+    REQUIRE(model.GetFeatureProb(1, 1, kUnshaded, 1) == Approx(1.0/4.0).epsilon(.01));
+    REQUIRE(model.GetFeatureProb(1, 2, kUnshaded, 1) == Approx(3.0/4.0).epsilon(.01));
+    REQUIRE(model.GetFeatureProb(2, 0, kUnshaded, 1) == Approx(3.0/4.0).epsilon(.01));
+    REQUIRE(model.GetFeatureProb(2, 1, kUnshaded, 1) == Approx(1.0/4.0).epsilon(.01));
+    REQUIRE(model.GetFeatureProb(2, 2, kUnshaded, 1) == Approx(3.0/4.0).epsilon(.01));
+  }
 }
 
 TEST_CASE("CalcLikelihoodScore", "[likelihood_score]") {
