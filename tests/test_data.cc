ab81e0044d120d9838c764d4da8074b686c47ffb
sumincho22 <scyclone.cho@gmail.com>
2021-04-14 16:07:42 -0500

    Updated tests for Image and Data classes

--- tests/test_data.cc
+++ tests/test_data.cc
@@ -9,6 +9,7 @@ const size_t kImageSize2 = 4;
 const size_t kNumImages = 4;
 const size_t kLabel = 1;
 
+const std::string kFileEmptyData = "../../../data/emptydata.txt";
 const std::string kFilePath3by3 = "../../../data/testdata_size3.txt";
 const std::string kFilePath4by4 = "../../../data/testdata_size4.txt";
 
@@ -21,13 +22,18 @@ TEST_CASE("Data operator overload", "[operator]") {
     REQUIRE(data.GetImages().size() == kNumImages);
   }
 
-  SECTION("Check number of images in a certain class") {
-    REQUIRE(data.GetNumImagesInClass(kLabel) == 2);
-  }
-
   SECTION("Check image size") {
     REQUIRE(data.GetImages()[0].GetSize() == kImageSize);
   }
+}
+
+TEST_CASE("Check empty dataset") {
+  naivebayes::Data empty_data(0);
+  std::ifstream empty_file(kFileEmptyData);
+  empty_file >> empty_data;
+
+  REQUIRE(empty_data.GetImageSize() == 0);
+  REQUIRE(empty_data.GetImages().size() == 0);
 }
 
 TEST_CASE("Check 4 by 4 dataset", "[4by4]") {
@@ -38,4 +44,32 @@ TEST_CASE("Check 4 by 4 dataset", "[4by4]") {
   REQUIRE(data.GetImageSize() == kImageSize2);
   REQUIRE(data.GetImages().size() == kNumImages);
   REQUIRE(data.GetImages()[1].GetLabel() == 2);
+}
+
+TEST_CASE("Check each image in dataset", "[images]") {
+  naivebayes::Data data(kImageSize);
+  std::ifstream input_file(kFilePath3by3);
+  input_file >> data;
+
+  REQUIRE(data.GetImages()[0].GetLabel() == 1);
+  REQUIRE(data.GetImages()[1].GetLabel() == 7);
+  REQUIRE(data.GetImages()[2].GetLabel() == 0);
+  REQUIRE(data.GetImages()[3].GetLabel() == 1);
+}
+
+TEST_CASE("GetNumImagesInClass") {
+  naivebayes::Data data(kImageSize);
+  std::ifstream input_file(kFilePath3by3);
+  input_file >> data;
+
+  SECTION("Check for class label without images") {
+    REQUIRE(data.GetNumImagesInClass(9) == 0);
+    REQUIRE(data.GetNumImagesInClass(2) == 0);
+  }
+
+  SECTION("Check each label for number of images") {
+    REQUIRE(data.GetNumImagesInClass(0) == 1);
+    REQUIRE(data.GetNumImagesInClass(1) == 2);
+    REQUIRE(data.GetNumImagesInClass(7) == 1);
+  }
 }
--- tests/test_image.cc
+++ tests/test_image.cc
@@ -1,20 +1,33 @@
 #include <catch2/catch.hpp>
 
+#include "core/pixel.h"
 #include "core/image.h"
 
-const size_t kUnshaded = 0;
-const size_t kShaded = 1;
-
+const size_t kImageSize = 3;
 const size_t kLabel = 1;
 
+const std::vector<std::vector<size_t>> kPixels
+    {
+        {kUnshaded, kShaded, kUnshaded},
+        {kUnshaded, kShaded, kUnshaded},
+        {kUnshaded, kShaded, kUnshaded}
+    };
+
 TEST_CASE("Image Constructor", "[image_constructor]") {
-//  const std::vector<std::vector<size_t>> pixels
-//  {
-//      {kUnshaded, kShaded, kUnshaded},
-//      {kUnshaded, kShaded, kUnshaded},
-//      {kUnshaded, kShaded, kUnshaded}
-//  };
-//  naivebayes::Image image(kLabel, pixels);
-//  REQUIRE(image.GetLabel() == kLabel);
-//  REQUIRE(image.GetPixels() == pixels);
+  naivebayes::Image image(kLabel, kImageSize, kPixels);
+  REQUIRE(image.GetLabel() == kLabel);
+  REQUIRE(image.GetPixels() == kPixels);
+}
+
+TEST_CASE("GetShade") {
+  naivebayes::Image image(kLabel, kImageSize, kPixels);
+  REQUIRE(image.GetShade(0, 0) == kUnshaded);
+  REQUIRE(image.GetShade(0, 1) == kShaded);
+  REQUIRE(image.GetShade(0, 2) == kUnshaded);
+  REQUIRE(image.GetShade(1, 0) == kUnshaded);
+  REQUIRE(image.GetShade(1, 1) == kShaded);
+  REQUIRE(image.GetShade(1, 2) == kUnshaded);
+  REQUIRE(image.GetShade(2, 0) == kUnshaded);
+  REQUIRE(image.GetShade(2, 1) == kShaded);
+  REQUIRE(image.GetShade(2, 2) == kUnshaded);
 }
