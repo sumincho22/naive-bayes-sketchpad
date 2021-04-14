fc6f0322e72279c208346784418ad86c703c3238
sumincho22 <scyclone.cho@gmail.com>
2021-04-14 16:16:48 -0500

    Implemented a test for CalcAccuracy method in Model class

--- tests/test_model.cc
+++ tests/test_model.cc
@@ -7,7 +7,6 @@
 const size_t kImageSize = 3;
 const size_t kNumImages = 4;
 const size_t kLabel = 1;
-const size_t kNumLabels = 3;
 
 TEST_CASE("Train", "[train]") {
   naivebayes::Data data(kImageSize);
@@ -33,4 +32,27 @@ TEST_CASE("Train", "[train]") {
     REQUIRE(model.GetFeatureProb(2, 1, Pixel::kUnshaded, kLabel) == Approx(1.0/4.0).epsilon(.01));
     REQUIRE(model.GetFeatureProb(2, 2, Pixel::kUnshaded, kLabel) == Approx(3.0/4.0).epsilon(.01));
   }
+}
+
+TEST_CASE("CalcAccuracy", "[calc_accuracy]") {
+  naivebayes::Data data(28);
+  std::ifstream input_file("../../../data/trainingimagesandlabels.txt");
+  input_file >> data;
+  naivebayes::Model model(data);
+  model.Train();
+
+  naivebayes::Data test_data(28);
+  std::ifstream test_file("../../../data/testimagesandlabels.txt");
+  test_file >> test_data;
+
+  double num_correct = 0;
+  for (const naivebayes::Image& image : test_data.GetImages()) {
+    if (model.Classify(image.GetPixels()) == image.GetLabel()) {
+      num_correct++;
+    }
+  }
+
+  double accuracy = model.CalcAccuracy(test_data.GetImages());
+  REQUIRE(accuracy >= 0.7);
+  REQUIRE(accuracy == num_correct / test_data.GetImages().size());
 }
